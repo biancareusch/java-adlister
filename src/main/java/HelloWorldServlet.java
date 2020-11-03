1ca37ce0e19d1bb0474de08e7898b3f41c9d969e
Bianca Reusch <reusch.bianca@gmail.com>
2020-11-03 09:54:15 -0600

    finished page counter + bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<h1>Hello, World!</h1>");
+        String name = req.getParameter("name");
+        out.println("<h3>Hello " + name + "</h3>");
     }
 }
--- src/main/java/ShowStudentServlet.java
+++ src/main/java/ShowStudentServlet.java
@@ -6,27 +6,30 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "ShowStudentServlet", urlPatterns = "/users")
+@WebServlet(name = "ShowStudentServlet", urlPatterns = "/counter")
 public class ShowStudentServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+    public static int count = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        String name = request.getParameter("name");
-        out.println("<h3>Hello " + name + "</h3>");
+        count++;
+        out.println("<h2>Page counter: " + count + "</h2>");
 
-        int grade = Integer.parseInt(request.getParameter("grade"));
-        out.println("<span>Grade: " + grade + ".00</span>");
+        String reset = request.getParameter("reset");
+
+        if (request.getParameterMap().containsKey("reset")) {
+            out.println("<h1>You've reset the Counter </h1>");
+            count = 0;
+        }
     }
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//
+//
+//       }
 
-        String name = request.getParameter("name");
-        out.println("<h1>As a post</h1>");
-        out.println("<h3>Hello " + name + "</h3>");
 
-        int grade = Integer.parseInt(request.getParameter("grade"));
-        out.println("<span>Grade: " + grade + ".00</span>");
-    }
 }
