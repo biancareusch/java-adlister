70caf723cf253b01a87e60d46e51ceb1d8a18fec
Bianca Reusch <reusch.bianca@gmail.com>
2020-11-12 16:37:02 -0600

    password exercise finished

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.models;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class User {
     private long id;
     private String username;
@@ -7,13 +9,13 @@ public class User {
     private String password;
 
     public User() {}
-
+//INSERT
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
-        this.password = password;
+        setPassword(password);
     }
-
+    //SELECT
     public User(long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
@@ -50,6 +52,6 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = BCrypt.hashpw(password,BCrypt.gensalt());
     }
 }
